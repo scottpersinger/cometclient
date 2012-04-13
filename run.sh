73d250381d4041e0fe3ddb8373fc62fc520e5852
Scott Persinger <scottp@berkeleyzone.net>
2012-04-13 11:22:44 -0700

    Corrected to use parameters on the command line

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export CLASSPATH=$CLASSPATH:./bayeux-api-2.3.1.jar:./cometd-java-client-2.3.1.jar:./cometd-java-common-2.3.1.jar:./jetty-client-7.4.4.v20110707.jar:./jetty-http-7.4.4.v20110707.jar:./jetty-io-7.4.4.v20110707.jar:./jetty-util-7.4.4.v20110707.jar:./bin
 echo $CLASSPATH
-java demo.StreamingEventEcho "scottpersinger@gmail.com" "secret2012d9IY18F1wYB5B0dKce0Ur0xbI" cm.Contact cm.Account
+echo "Usage: StreamingEventEcho <username> <password:token> <topic1> <topic2> ..."
+java demo.StreamingEventEcho $@
