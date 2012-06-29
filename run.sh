1b67e314e6c06d7d950b53b70dd58f9d867f3cff
Scott Persinger <scottpersinger@gmail.com>
2012-06-29 10:56:58 -0700

    Enabled oauth auth and reloading of streaming object list

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export CLASSPATH=$CLASSPATH:./bayeux-api-2.3.1.jar:./cometd-java-client-2.3.1.jar:./cometd-java-common-2.3.1.jar:./jetty-client-7.4.4.v20110707.jar:./jetty-http-7.4.4.v20110707.jar:./jetty-io-7.4.4.v20110707.jar:./jetty-util-7.4.4.v20110707.jar:./bin
+export CLASSPATH=$CLASSPATH:./bayeux-api-2.3.1.jar:./cometd-java-client-2.3.1.jar:./cometd-java-common-2.3.1.jar:./jetty-client-7.4.4.v20110707.jar:./jetty-http-7.4.4.v20110707.jar:./jetty-io-7.4.4.v20110707.jar:./jetty-util-7.4.4.v20110707.jar:./mysql-connector-java-5.1.20-bin.jar:./gson-2.1.jar:./rabbitmq-client.jar:./bin
 echo $CLASSPATH
-echo "Usage: StreamingEventEcho <username> <password:token> <topic1> <topic2> ..."
-java demo.StreamingEventEcho $@
+echo "Usage: StreamingEventEcho <dburl> <rabbiturl> <clientId> <clientSecret>"
+#java demo.StreamingEventEcho $@
+java demo.StreamingEventEcho "jdbc:mysql://localhost/cloud5?user=root&password=" "amqp://localhost" "3MVG9rFJvQRVOvk7dcW8zRXD4lv7ZdT638O2Ti7hrGGo2Oa7iFCoyabLryNhGv_j2x9yo0Ea6.VmqzUR6LDgD" "7176140918132069100"
--- src/demo/StreamingEventEcho.java
+++ src/demo/StreamingEventEcho.java
@@ -15,6 +15,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -31,6 +33,8 @@ import org.cometd.client.transport.ClientTransport;
 import org.cometd.client.transport.LongPollingTransport;
 import org.eclipse.jetty.client.ContentExchange;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.util.StringUtil;
+import org.eclipse.jetty.util.UrlEncoded;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -60,6 +64,8 @@ public class StreamingEventEcho implements Runnable {
     private static String dbpass;
     static String dbhost;
     static String dbname;
+    static String clientId;
+    static String clientSecret;
     static ArrayList<StreamingEventEcho> _listeners = new ArrayList<StreamingEventEcho>();
     static Timer reconnectTimer = new Timer(true);
     static BlockingQueue<Object[]> publishQueue = new LinkedBlockingQueue<Object[]>();
@@ -69,24 +75,36 @@ public class StreamingEventEcho implements Runnable {
         // The path for the Streaming API endpoint
     private static final String DEFAULT_PUSH_ENDPOINT = "/cometd/24.0";
     private static ExecutorService _executor = null; 
-
+    private static String dbUrl;
+    
     static class SalesforceConfig {
     	String username;
     	String password;
     	String token;
     	
     }
+
+    static class SalesforceOauth {
+        String access_token;
+        String endpoint;
+        String refresh_token;
+        String instance_url;        
+    }
+    
     public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            System.err.println("Usage: StreamingEventEcho <dbURL> <rabbitURL>");
+        if (args.length < 4) {
+            System.err.println("Usage: StreamingEventEcho <dbURL> <rabbitURL> <clientId> <clientSecret>");
             System.err.println("dbUrl format: jdbc:mysql://host/database?user=user&password=password");
             System.err.println("rabbitUrl format: amqp://userName:password@hostName:portNumber/virtualHost");
             System.exit(-1);
         }
         setNoValidation();
 
+        clientId = args[2];
+        clientSecret = args[3];
+        
         // Load the initial tenant list
-        final String dbUrl = args[0];
+        dbUrl = args[0];
     	loadTenants(dbUrl);
 
     	// Now connect to Rabbit MQ
@@ -147,6 +165,14 @@ public class StreamingEventEcho implements Runnable {
 							e.printStackTrace();
 						}
 	                	runListeners();
+	                } else if (command.matches("restart:\\d+")) {
+	                	Matcher m = Pattern.compile("restart:(\\d+)").matcher(command);
+	                	if (!m.matches()) {
+	                		System.out.println("That didnt match");
+	                		m = Pattern.compile(".*(\\d+).*").matcher(command);
+	                	}
+	                	String tid = m.group(1);
+	                	restartListener(Long.parseLong(tid));
 	                }
 	                // (process the message components here ...)
 	                channel.basicAck(deliveryTag, false);
@@ -177,29 +203,31 @@ public class StreamingEventEcho implements Runnable {
     	    Statement stmt = conn.createStatement();
     	    // Load the set of Salesforce connection credentials per tenant
     	    if (stmt.execute("select mirror_tenant.id,name,payload_json from mirror_tenant inner join mirror_keyvaluerecord on " + 
-    	    "mirror_tenant.id = mirror_keyvaluerecord.tenant_id where mirror_keyvaluerecord.`key` = 'salesforce_config'")) {
+    	    "mirror_tenant.id = mirror_keyvaluerecord.tenant_id " + 
+    	    		"where mirror_keyvaluerecord.`key` = 'salesforce_config' " +
+    	    		"OR mirror_keyvaluerecord.`key` = 'salesforce_oauth'")) {
     	    	ResultSet rs = stmt.getResultSet();
     	    	while (rs.next()) {
 	    	    	long tenantId = rs.getLong("id");
 	    	    	String tenantName = rs.getString("name");
 	    	    	String json = rs.getString("payload_json");
 	    	    	Gson gson = new Gson();
-	    	    	SalesforceConfig sfConfig = gson.fromJson(json, SalesforceConfig.class);
-	    	    	// Now load the list of objects to listen to
-	    	    	Statement objSt = conn.createStatement();
-	    	    	String sql = "select `key` from mirror_keyvaluerecord where tenant_id = " + tenantId + " and `key` like 'salesforce.mappings.%'";
-	    	    	ArrayList<String> objectList = new ArrayList<String>();
-	    	    	if (objSt.execute(sql)) {
-	    	    		ResultSet rs2 = objSt.getResultSet();
-	    	    		while (rs2.next()) {
-	    	    			// Grab object name off the end of the key
-	    	    			// FIXME: We should record the topic name in the db and use that here
-	    	    			objectList.add("cm." + rs2.getString(1).split("\\.")[2]);
+	    	    	// Try to parse as salesforce_config or salesforce_oauth
+	    	    	SalesforceConfig sfConfig = null;
+	    	    	SalesforceOauth sfOauth = null;
+	    	    	try {
+	    	    		sfConfig = gson.fromJson(json, SalesforceConfig.class);
+	    	    		if (sfConfig.username == null) {
+	    	    			sfConfig = null;
+		    	    		sfOauth = gson.fromJson(json, SalesforceOauth.class);
 	    	    		}
+	    	    	} catch (Exception e) {
+	    	    		sfOauth = gson.fromJson(json, SalesforceOauth.class);
 	    	    	}
-	    	    	objSt.close();
+	    	    	// Now load the list of objects to listen to
+	    	    	ArrayList<String> objectList = loadObjectList(tenantId, conn);
 	    	    	System.out.println("Found tenant " + tenantId + " (" + tenantName + ")");
-	    	    	_listeners.add(new StreamingEventEcho(tenantId, tenantName, sfConfig, objectList));
+	    	    	_listeners.add(new StreamingEventEcho(tenantId, tenantName, sfConfig, sfOauth, objectList));
     	    	}
     	    }
     	    
@@ -214,6 +242,29 @@ public class StreamingEventEcho implements Runnable {
     	}    	
     }    	
 
+    public static ArrayList<String> loadObjectList(long tenantId, Connection conn) throws SQLException {
+    	if (conn == null) {
+    		conn = DriverManager.getConnection(dbUrl);
+    	}
+    	Statement objSt = conn.createStatement();
+    	// This is a hack. We just jam 'streaming' into the LIKE for the payload to filter for objects
+    	// that have streaming enabled. If someone stored "streaming":"false" then our logic would fail.
+    	String sql = "select `key` from mirror_keyvaluerecord where tenant_id = " + tenantId 
+    			+ " and `key` like 'salesforce.mappings.%'"
+    			+ " and payload_json like '%streaming%'";
+    	ArrayList<String> objectList = new ArrayList<String>();
+    	if (objSt.execute(sql)) {
+    		ResultSet rs2 = objSt.getResultSet();
+    		while (rs2.next()) {
+    			// Grab object name off the end of the key
+    			// FIXME: We should record the topic name in the db and use that here
+    			objectList.add("cm." + rs2.getString(1).split("\\.")[2]);
+    		}
+    	}
+    	objSt.close();
+    	
+    	return objectList;
+    }
 
     private static void runListeners() {
         _executor = Executors.newCachedThreadPool();
@@ -238,57 +289,110 @@ public class StreamingEventEcho implements Runnable {
 		}
     }
     
-    private long _tenantId;
-    private String _tenantName;
-    private SalesforceConfig _sfConfig;
+    private static void restartListener(long tenant_id) {
+    	for (int i = 0; i < _listeners.size(); i++) {
+    		StreamingEventEcho listener = _listeners.get(i);
+    		if (listener._tenantId == tenant_id) {
+    			listener.stop();
+        		_listeners.remove(i);
+        		// Reload list of objects to listen to
+        		try {
+					ArrayList<String> objectList = loadObjectList(tenant_id, null);
+					StreamingEventEcho newListener = new StreamingEventEcho(listener._tenantId, listener._tenantName, 
+	    	    			listener._sfConfig, listener._sfOauth, objectList); 
+	    	    	_listeners.add(newListener);
+	    	    	_executor.execute(newListener);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        		
+    		}
+    		break;
+    	}
+    }
+    
+    public long _tenantId;
+    public String _tenantName;
+    public SalesforceConfig _sfConfig;
+    public SalesforceOauth _sfOauth;
     private List<String> _topics;
     private boolean _keepRunning;
+    public String _sid;
+    public boolean _connectFailed401;
     
-    public StreamingEventEcho(long tenantId, String tenantName, SalesforceConfig sfConfig, List<String> topics) {
+    public StreamingEventEcho(long tenantId, String tenantName, SalesforceConfig sfConfig, 
+    		SalesforceOauth sfOauth, List<String> topics) {
     	_tenantId = tenantId;
     	_tenantName = tenantName;
     	_sfConfig = sfConfig;
+    	_sfOauth = sfOauth;
     	_topics = topics;
     }
     
 	@Override
 	public void run() {
 		_keepRunning = true;
-		
 		try {
-			// TODO Auto-generated method stub
-			System.out.println("Streaming listener for tenant " + _tenantName + "(" + _tenantId + ")");
-			System.out.println(_tenantId + ": Listening for topics: " + _topics.toString());
-	
-			System.out.println(_tenantId + ": Connecting to Salesforce: " + _sfConfig.username + " / " + _sfConfig.password + ":" + _sfConfig.token);
-	        BayeuxClient client = getClient(_sfConfig.username, _sfConfig.password + _sfConfig.token);
-	        client.handshake();
-	
-	        System.out.println("Waiting for handshake");
-	        waitForHandshake(client, 60 * 1000, 1000);
-	
-	        for (String topic : _topics) {
-		        System.out.println(_tenantId + ": Subscribing to topic: " + topic);
-		        client.getChannel("/topic/" + topic).subscribe(new MessageListener() {
-		            @Override
-		            public void onMessage(ClientSessionChannel channel, Message message) {
-	                	System.out.println(_tenantId + ": [EVENT] " + message.getJSON());
-	                	Object[] payload = {_tenantId, message.getJSON()};
-	                	publishQueue.add(payload);
-		            }
-		        });
-	        }
-	        System.out.println(_tenantId + ": READY for streamed data from Force.com...");
-	        while (_keepRunning) {
-	            // This infinite loop is for demo only, to receive streamed events
-	            // on the specified topic from Salesforce.com
-	            Thread.sleep(1000);
-	        }
-	        System.out.println(_tenantId + ": stopping");
-	        for (String topic : _topics) {
-		        client.getChannel("/topic/" + topic).unsubscribe();
-	        }
-	        client.disconnect();
+			while (_keepRunning) {
+				// TODO Auto-generated method stub
+				System.out.println("Streaming listener for tenant " + _tenantName + "(" + _tenantId + ")");
+				System.out.println(_tenantId + ": Listening for topics: " + _topics.toString());
+		
+				BayeuxClient client = null;
+				if (_sfConfig != null) {
+					System.out.println(_tenantId + ": Connecting to Salesforce: " + _sfConfig.username + " / " + _sfConfig.password + ":" + _sfConfig.token);
+					client = getClient(_sfConfig.username, _sfConfig.password + _sfConfig.token, null);
+				} else if (_sfOauth != null){
+					System.out.println(_tenantId + ": Connecting to Salesforce by Oauth: " + _sfOauth.access_token + " / " + _sfOauth.instance_url);
+					client = getClient(null, null, _sfOauth);
+				} else {
+					throw new Exception("Error, no SalesforceConfig or SalesforceOauth available");
+				}
+				_connectFailed401 = false;
+		        client.handshake();
+		
+		        System.out.println("Waiting for handshake");
+		        try {
+		        	waitForHandshake(client, 20 * 1000, 1000);
+		        } catch (IllegalStateException e) {
+		        	System.out.println("Got an Illegal state after handshake:" + e);
+		        	client.abort();
+		        	if (_sfOauth != null) {
+		        		// Request a new access_token using our refresh_token
+		        		JSONObject jsonResult = getNewAccessToken(_sfOauth);
+		        		System.out.println("JSON response: " + jsonResult.toString());
+		        		_sfOauth.access_token = jsonResult.getString("access_token");
+		        		// FIXME: We should store the new access_token in the db
+		        		// loop back and try to auth again
+		        		continue;
+		        	}
+		        	return;
+		        }
+		
+		        for (String topic : _topics) {
+			        System.out.println(_tenantId + ": Subscribing to topic: " + topic);
+			        client.getChannel("/topic/" + topic).subscribe(new MessageListener() {
+			            @Override
+			            public void onMessage(ClientSessionChannel channel, Message message) {
+		                	System.out.println(_tenantId + ": [EVENT] " + message.getJSON());
+		                	Object[] payload = {_tenantId, message.getJSON()};
+		                	publishQueue.add(payload);
+			            }
+			        });
+		        }
+		        System.out.println(_tenantId + ": READY for streamed data from Force.com...");
+		        while (_keepRunning) {
+		            // This infinite loop is for demo only, to receive streamed events
+		            // on the specified topic from Salesforce.com
+		            Thread.sleep(1000);
+		        }
+		        System.out.println(_tenantId + ": stopping");
+		        for (String topic : _topics) {
+			        client.getChannel("/topic/" + topic).unsubscribe();
+		        }
+		        client.disconnect();
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -300,18 +404,24 @@ public class StreamingEventEcho implements Runnable {
 		_keepRunning =false;
 	}
 	
-    private BayeuxClient getClient(String username, String password) throws Exception {
+    private BayeuxClient getClient(String username, String password, SalesforceOauth sfOauth) throws Exception {
         HttpClient authClient = new HttpClient();
         authClient.start();
+
+    	String instance_url;
         
-    	// Use old style SOAP login that only needs username/password
-    	String[] pair = SoapLoginUtil.login(authClient, username, password);
-		if (pair == null) {
-			throw new RuntimeException("Salesforce authentication failed");
-		}
-    	final String sid = pair[0];
-    	String instance_url = pair[1];
-		
+        if (username != null && password != null) {
+	    	// Use old style SOAP login that only needs username/password
+	    	String[] pair = SoapLoginUtil.login(authClient, username, password);
+			if (pair == null) {
+				throw new RuntimeException("Salesforce authentication failed");
+			}
+	    	_sid = pair[0];
+	    	instance_url = pair[1];
+        } else {
+        	_sid = sfOauth.access_token;
+        	instance_url = sfOauth.instance_url;
+        }
 		/*
         // Authenticate via OAuth
         JSONObject response = oauthLogin();
@@ -340,13 +450,21 @@ public class StreamingEventEcho implements Runnable {
             @Override 
             protected void customize(ContentExchange exchange) {
                 super.customize(exchange);
-                exchange.addRequestHeader("Authorization", "OAuth " + sid);
+                exchange.addRequestHeader("Authorization", "OAuth " + StreamingEventEcho.this._sid);
             }
         };
 
         // Now set up the Bayeux client itself
         BayeuxClient client = new BayeuxClient(instance_url
-                + DEFAULT_PUSH_ENDPOINT, transport);
+                + DEFAULT_PUSH_ENDPOINT, transport) {
+        	public void onFailure(Throwable xx, Message[] messages) {
+        		System.out.println("BayeuxClient error: "+ xx.getMessage());
+        		if (xx.getMessage().indexOf("response 401") >= 0) {
+        			System.out.println("Detecting bad auth");
+        			StreamingEventEcho.this._connectFailed401 = true;
+        		}
+        	}
+        };
 
         return client;
     }
@@ -356,6 +474,9 @@ public class StreamingEventEcho implements Runnable {
         long start = System.currentTimeMillis();
         long end = start + timeoutInMilliseconds;
         while (System.currentTimeMillis() < end) {
+        	if (_connectFailed401) {
+        		throw new IllegalStateException("AUth failed");
+        	}
             if (client.isHandshook())
                 return;
             try {
@@ -430,4 +551,41 @@ public class StreamingEventEcho implements Runnable {
 
     }**/
 
+    private JSONObject getNewAccessToken(SalesforceOauth sfOauth) throws Exception {
+        HttpClient httpClient = new HttpClient();
+        httpClient.start();
+
+        String url = LOGIN_SERVER + "/services/oauth2/token";
+
+        ContentExchange exchange = new ContentExchange();
+        exchange.setMethod("POST");
+        exchange.setURL(url);
+
+        UrlEncoded params = new UrlEncoded();
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", sfOauth.refresh_token);
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("format", "json");
+        
+        String message = "grant_type=refresh_token"
+        		+ "&client_id=" + clientId
+                + "&client_secret=" + clientSecret
+                + "&refresh_token=" + sfOauth.refresh_token.replace("=", "%3D")
+                + "&format=json";
+
+        message = "client_secret=7176140918132069100&grant_type=refresh_token&client_id=3MVG9rFJvQRVOvk7dcW8zRXD4lv7ZdT638O2Ti7hrGGo2Oa7iFCoyabLryNhGv_j2x9yo0Ea6.VmqzUR6LDgD&refresh_token=5Aep861.EkZJuT7_lvUke.TdvF45yyzDcG.5kCM04ffwz31rauYNIMGGMWnsQJtiI8uGxRTh43L7Q%3D%3D&format=json";
+        System.out.println("POSTING: " + message);
+        
+        //exchange.setRequestHeader("Content-Type",
+        //        "application/x-www-form-urlencoded");
+        exchange.setRequestContentSource(new ByteArrayInputStream(message
+                .getBytes("UTF-8")));
+        //exchange.setRequestContentType("application/x-www-form-urlencoded;charset=utf-8");
+
+        httpClient.send(exchange);
+        exchange.waitForDone();
+
+        return new JSONObject(new JSONTokener(exchange.getResponseContent()));
+    }
 }
